278e9186cef10bcacd0394d07f76b455fb39276c
Pierre Oblin <pierre.oblin@orange.com>
2020-03-01 15:23:23 +0000

    add package with wireguard module and tools

---
+++ addblob.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+rm -rf src/*
+mkdir -p src/wireguard-src
+cd src/wireguard-src
+
+#get wireguard sources
+git clone https://git.zx2c4.com/wireguard-linux-compat
+cd wireguard-linux-compat
+git checkout v0.0.20200215 
+cd ..
+
+#get wireguard tools sources
+git clone https://git.zx2c4.com/wireguard-tools
+cd wireguard-tools
+git checkout v1.0.20200206
+cd ..
+
+tar cvf ../wireguard-sources.tgz *
+
+cd ../..
+pwd
+bosh add-blob ./src/wireguard-sources.tgz  wireguard/wireguard-sources.tgz
+
