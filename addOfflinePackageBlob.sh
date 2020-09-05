d8cf3c7f573f266ff1020031252d648753f17e96
Pierre Oblin <pierre.oblin@orange.com>
2020-09-05 19:11:29 +0000

    bump wireguard for 621.82 compatibility

--- addOfflinePackageBlob.sh
+++ addOfflinePackageBlob.sh
@@ -6,15 +6,15 @@ mkdir -p src/wireguard-packages
 cd src/wireguard-packages
 
 #get wireguard sources
-apt-get download wireguard
+#apt-get download wireguard
 
 #get wireguard tools sources
-apt-get download wireguard-tools
-apt-get download wireguard-dkms
+#apt-get download wireguard-tools
+#apt-get download wireguard-dkms
 
 cd ..
 pwd
-bosh add-blob ./src/wireguard-packages/wireguard_1.0.20200513-1~16.04.1_all.deb wireguard/wireguard.deb 
-bosh add-blob ./src/wireguard-packages/wireguard-tools_1.0.20200513-1~16.04.1_amd64.deb  wireguard-tools.deb
+bosh add-blob ./src/wireguard-packages/wireguard_1.0.20200513-1~16.04.2_all.deb     wireguard/wireguard.deb 
+bosh add-blob ./src/wireguard-packages/wireguard-tools_1.0.20200513-1~16.04.2_amd64.deb  wireguard/wireguard-tools.deb
 bosh add-blob ./src/wireguard-packages/wireguard-dkms_1.0.20200611-1ubuntu1~16.04.1_all.deb wireguard/wireguard-dkms.deb
 
