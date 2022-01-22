fc1f6899c153e7079036de600b67b5f13de46f02
Pierre Oblin <pierre.oblin@orange.com>
2022-01-22 15:50:12 +0000

    remove mtr

--- addblobs-tools.sh
+++ addblobs-tools.sh
@@ -2,7 +2,7 @@
 set -x
 
 bosh add-blob ./src/ntop.tar.gz  ntop/ntop.tar.gz
-bosh add-blob ./src/debs/iftop/iftop_1.0~pre4-4_amd64.deb  iftop/iftop.deb
 
+bosh add-blob ./src/debs/iftop/iftop_1.0~pre4-4_amd64.deb  iftop/iftop.deb
 
 
